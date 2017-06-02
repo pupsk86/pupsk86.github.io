05d200fc47a4949a2be940917958f36ae186e850
gridasov <gridasov@gridasov.neyvabank.ru>
2017-06-02 11:31:50 +0600

    Add draw hydropost image

---
+++ public/js/common.js
@@ -0,0 +1,16 @@
+$(function(){
+    $preview = $('#preview');
+    $hydropost = $('select[name=hydropost]');
+    
+    function drawHydropost(hydropostId) {
+        var src = hydropostId ?
+            'http://gis.vodinfo.ru/informer/draw/v2_' + hydropostId + '_250_200_30_ffffff_110_8_7_H_none.png' :
+            '';
+        $preview.attr('src', src);
+    }
+
+    $hydropost.change(function(){
+        var hydropostId = $(this).val;
+        drawHydropost(hydropostId);
+    });
+});
