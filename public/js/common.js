5b0246c70973b44815664cad8dc58a93d54d95c3
gridasov <gridasov@gridasov.neyvabank.ru>
2017-06-02 12:23:07 +0600

    Some style changes

--- public/js/common.js
+++ public/js/common.js
@@ -4,7 +4,7 @@ $(function(){
     
     function drawHydropost(hydropostId) {
         var src = hydropostId ?
-            'http://gis.vodinfo.ru/informer/draw/v2_' + hydropostId + '_250_200_30_ffffff_110_8_7_H_none.png' :
+            'http://gis.vodinfo.ru/informer/draw/v2_' + hydropostId + '_400_300_30_ffffff_110_8_7_H_none.png' :
             '';
         $preview.attr('src', src);
     }
